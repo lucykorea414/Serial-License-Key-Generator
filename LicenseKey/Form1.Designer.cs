fc7b458c9d99e6a7eddb692771ed65374ada0fb9
lucykorea414 <31093793+lucykorea414@users.noreply.github.com>
2022-05-16 15:17:40 +0900

    update Form1 Designer

--- LicenseKey/Form1.Designer.cs
+++ LicenseKey/Form1.Designer.cs
@@ -28,12 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.btnRegistration = new System.Windows.Forms.Button();
+            this.btnAbout = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(105, 80);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(119, 23);
+            this.btnGenerate.TabIndex = 1;
+            this.btnGenerate.Text = "&Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            // 
+            // btnRegistration
+            // 
+            this.btnRegistration.Location = new System.Drawing.Point(230, 80);
+            this.btnRegistration.Name = "btnRegistration";
+            this.btnRegistration.Size = new System.Drawing.Size(119, 23);
+            this.btnRegistration.TabIndex = 2;
+            this.btnRegistration.Text = "&Registration";
+            this.btnRegistration.UseVisualStyleBackColor = true;
+            // 
+            // btnAbout
+            // 
+            this.btnAbout.Location = new System.Drawing.Point(355, 80);
+            this.btnAbout.Name = "btnAbout";
+            this.btnAbout.Size = new System.Drawing.Size(119, 23);
+            this.btnAbout.TabIndex = 3;
+            this.btnAbout.Text = "&About";
+            this.btnAbout.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(599, 198);
+            this.Controls.Add(this.btnAbout);
+            this.Controls.Add(this.btnRegistration);
+            this.Controls.Add(this.btnGenerate);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Button btnGenerate;
+        private Button btnRegistration;
+        private Button btnAbout;
     }
 }
