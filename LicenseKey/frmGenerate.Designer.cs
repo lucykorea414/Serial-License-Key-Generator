39e969de660f505181b2e63a9c6745c014aabc0c
lucykorea414 <31093793+lucykorea414@users.noreply.github.com>
2022-05-16 15:02:23 +0900

    update Form designer

--- LicenseKey/frmGenerate.Designer.cs
+++ LicenseKey/frmGenerate.Designer.cs
@@ -28,12 +28,126 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtProductID = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbLicenseType = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtExpDay = new System.Windows.Forms.TextBox();
+            this.txtProductKey = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(401, 145);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(119, 23);
+            this.btnGenerate.TabIndex = 0;
+            this.btnGenerate.Text = "&Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Product ID:";
+            // 
+            // txtProductID
+            // 
+            this.txtProductID.Location = new System.Drawing.Point(122, 23);
+            this.txtProductID.Name = "txtProductID";
+            this.txtProductID.Size = new System.Drawing.Size(398, 23);
+            this.txtProductID.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "License Type:";
+            // 
+            // cmbLicenseType
+            // 
+            this.cmbLicenseType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbLicenseType.FormattingEnabled = true;
+            this.cmbLicenseType.Location = new System.Drawing.Point(122, 54);
+            this.cmbLicenseType.Name = "cmbLicenseType";
+            this.cmbLicenseType.Size = new System.Drawing.Size(129, 23);
+            this.cmbLicenseType.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Experience days:";
+            // 
+            // txtExpDay
+            // 
+            this.txtExpDay.Location = new System.Drawing.Point(122, 85);
+            this.txtExpDay.Name = "txtExpDay";
+            this.txtExpDay.Size = new System.Drawing.Size(129, 23);
+            this.txtExpDay.TabIndex = 6;
+            // 
+            // txtProductKey
+            // 
+            this.txtProductKey.Location = new System.Drawing.Point(122, 116);
+            this.txtProductKey.Name = "txtProductKey";
+            this.txtProductKey.ReadOnly = true;
+            this.txtProductKey.Size = new System.Drawing.Size(398, 23);
+            this.txtProductKey.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(38, 119);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Product Key:";
+            // 
+            // frmGenerate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmGenerate";
+            this.ClientSize = new System.Drawing.Size(544, 184);
+            this.Controls.Add(this.txtProductKey);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtExpDay);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbLicenseType);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtProductID);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGenerate);
+            this.MaximizeBox = false;
+            this.Name = "frmGenerate";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Generate";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button btnGenerate;
+        private Label label1;
+        private TextBox txtProductID;
+        private Label label2;
+        private ComboBox cmbLicenseType;
+        private Label label3;
+        private TextBox txtExpDay;
+        private TextBox txtProductKey;
+        private Label label4;
     }
 }
