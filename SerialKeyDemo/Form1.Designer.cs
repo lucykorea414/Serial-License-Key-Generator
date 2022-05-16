065196cdf06ac475aaf570b4525327ca5e8ad713
lucykorea414 <31093793+lucykorea414@users.noreply.github.com>
2022-05-16 14:21:53 +0900

    Update Form1.Designer.cs

--- SerialKeyDemo/Form1.Designer.cs
+++ SerialKeyDemo/Form1.Designer.cs
@@ -28,45 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtDay = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.txtSerial = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.btnValid = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBox1
+            // txtDay
             // 
-            this.textBox1.Location = new System.Drawing.Point(78, 32);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(92, 23);
-            this.textBox1.TabIndex = 0;
+            this.txtDay.Location = new System.Drawing.Point(62, 14);
+            this.txtDay.Name = "txtDay";
+            this.txtDay.Size = new System.Drawing.Size(92, 23);
+            this.txtDay.TabIndex = 0;
+            this.txtDay.Text = "30";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(41, 35);
+            this.label1.Location = new System.Drawing.Point(25, 17);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(31, 15);
             this.label1.TabIndex = 1;
             this.label1.Text = "Day:";
             // 
-            // textBox2
+            // txtPassword
             // 
-            this.textBox2.Location = new System.Drawing.Point(247, 32);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(207, 23);
-            this.textBox2.TabIndex = 0;
+            this.txtPassword.Location = new System.Drawing.Point(231, 14);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(207, 23);
+            this.txtPassword.TabIndex = 0;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(181, 35);
+            this.label2.Location = new System.Drawing.Point(165, 17);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(60, 15);
             this.label2.TabIndex = 1;
@@ -75,69 +76,69 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(33, 66);
+            this.label3.Location = new System.Drawing.Point(17, 48);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(39, 15);
             this.label3.TabIndex = 3;
             this.label3.Text = "Serial:";
             // 
-            // textBox3
+            // txtSerial
             // 
-            this.textBox3.Location = new System.Drawing.Point(78, 61);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(376, 23);
-            this.textBox3.TabIndex = 2;
+            this.txtSerial.Location = new System.Drawing.Point(62, 43);
+            this.txtSerial.Name = "txtSerial";
+            this.txtSerial.Size = new System.Drawing.Size(376, 23);
+            this.txtSerial.TabIndex = 2;
             // 
-            // button1
+            // btnGenerate
             // 
-            this.button1.Location = new System.Drawing.Point(379, 90);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Generate";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnGenerate.Location = new System.Drawing.Point(363, 72);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 4;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(33, 124);
+            this.label4.Location = new System.Drawing.Point(17, 106);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(43, 15);
             this.label4.TabIndex = 6;
             this.label4.Text = "Status:";
             // 
-            // textBox4
+            // txtStatus
             // 
-            this.textBox4.Location = new System.Drawing.Point(78, 119);
-            this.textBox4.Multiline = true;
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(376, 184);
-            this.textBox4.TabIndex = 5;
+            this.txtStatus.Location = new System.Drawing.Point(62, 101);
+            this.txtStatus.Multiline = true;
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(376, 184);
+            this.txtStatus.TabIndex = 5;
             // 
-            // button2
+            // btnValid
             // 
-            this.button2.Location = new System.Drawing.Point(379, 309);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 7;
-            this.button2.Text = "Generate";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnValid.Location = new System.Drawing.Point(363, 291);
+            this.btnValid.Name = "btnValid";
+            this.btnValid.Size = new System.Drawing.Size(75, 23);
+            this.btnValid.TabIndex = 7;
+            this.btnValid.Text = "Valid";
+            this.btnValid.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(506, 367);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(454, 325);
+            this.Controls.Add(this.btnValid);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.btnGenerate);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.txtSerial);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtDay);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -147,15 +148,15 @@
 
         #endregion
 
-        private TextBox textBox1;
+        private TextBox txtDay;
         private Label label1;
-        private TextBox textBox2;
+        private TextBox txtPassword;
         private Label label2;
         private Label label3;
-        private TextBox textBox3;
-        private Button button1;
+        private TextBox txtSerial;
+        private Button btnGenerate;
         private Label label4;
-        private TextBox textBox4;
-        private Button button2;
+        private TextBox txtStatus;
+        private Button btnValid;
     }
 }
