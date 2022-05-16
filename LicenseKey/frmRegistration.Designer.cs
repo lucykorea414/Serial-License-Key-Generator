bee50e6de1e9ce1dea760d2f377e1165fd8d8566
lucykorea414 <31093793+lucykorea414@users.noreply.github.com>
2022-05-16 15:08:19 +0900

    update Registration Form Designer

--- LicenseKey/frmRegistration.Designer.cs
+++ LicenseKey/frmRegistration.Designer.cs
@@ -28,12 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtProductID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtProductKey = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtProductID
+            // 
+            this.txtProductID.Location = new System.Drawing.Point(103, 20);
+            this.txtProductID.Name = "txtProductID";
+            this.txtProductID.ReadOnly = true;
+            this.txtProductID.Size = new System.Drawing.Size(398, 23);
+            this.txtProductID.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Product ID:";
+            // 
+            // txtProductKey
+            // 
+            this.txtProductKey.Location = new System.Drawing.Point(103, 49);
+            this.txtProductKey.Name = "txtProductKey";
+            this.txtProductKey.Size = new System.Drawing.Size(398, 23);
+            this.txtProductKey.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 52);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Product Key:";
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(426, 78);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 11;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            // 
+            // frmRegistration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmRegistration";
+            this.ClientSize = new System.Drawing.Size(523, 120);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtProductKey);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtProductID);
+            this.Controls.Add(this.label1);
+            this.MinimizeBox = false;
+            this.Name = "frmRegistration";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Registration";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private TextBox txtProductID;
+        private Label label1;
+        private TextBox txtProductKey;
+        private Label label4;
+        private Button btnOK;
     }
 }
