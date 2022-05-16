e543f8b7e6436cfba2649165b298d419f0bb4ca4
lucykorea414 <31093793+lucykorea414@users.noreply.github.com>
2022-05-16 15:15:10 +0900

    update About Form Designer

--- LicenseKey/frmAbout.Designer.cs
+++ LicenseKey/frmAbout.Designer.cs
@@ -28,12 +28,131 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblProductName = new System.Windows.Forms.Label();
+            this.lblProductID = new System.Windows.Forms.Label();
+            this.lblProductKey = new System.Windows.Forms.Label();
+            this.lblLicenseType = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(174, 182);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 12;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 15);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Product Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(54, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 15);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Product ID:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 15);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Product Key:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(44, 131);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 15);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "License Type:";
+            // 
+            // lblProductName
+            // 
+            this.lblProductName.AutoSize = true;
+            this.lblProductName.Location = new System.Drawing.Point(138, 41);
+            this.lblProductName.Name = "lblProductName";
+            this.lblProductName.Size = new System.Drawing.Size(13, 15);
+            this.lblProductName.TabIndex = 14;
+            this.lblProductName.Text = "?";
+            // 
+            // lblProductID
+            // 
+            this.lblProductID.AutoSize = true;
+            this.lblProductID.Location = new System.Drawing.Point(138, 70);
+            this.lblProductID.Name = "lblProductID";
+            this.lblProductID.Size = new System.Drawing.Size(13, 15);
+            this.lblProductID.TabIndex = 14;
+            this.lblProductID.Text = "?";
+            // 
+            // lblProductKey
+            // 
+            this.lblProductKey.AutoSize = true;
+            this.lblProductKey.Location = new System.Drawing.Point(138, 101);
+            this.lblProductKey.Name = "lblProductKey";
+            this.lblProductKey.Size = new System.Drawing.Size(13, 15);
+            this.lblProductKey.TabIndex = 14;
+            this.lblProductKey.Text = "?";
+            // 
+            // lblLicenseType
+            // 
+            this.lblLicenseType.AutoSize = true;
+            this.lblLicenseType.Location = new System.Drawing.Point(138, 131);
+            this.lblLicenseType.Name = "lblLicenseType";
+            this.lblLicenseType.Size = new System.Drawing.Size(13, 15);
+            this.lblLicenseType.TabIndex = 14;
+            this.lblLicenseType.Text = "?";
+            // 
+            // frmAbout
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmAbout";
+            this.ClientSize = new System.Drawing.Size(421, 221);
+            this.Controls.Add(this.lblLicenseType);
+            this.Controls.Add(this.lblProductKey);
+            this.Controls.Add(this.lblProductID);
+            this.Controls.Add(this.lblProductName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnOK);
+            this.MaximizeBox = false;
+            this.Name = "frmAbout";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "About";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button btnOK;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label lblProductName;
+        private Label lblProductID;
+        private Label lblProductKey;
+        private Label lblLicenseType;
     }
 }
