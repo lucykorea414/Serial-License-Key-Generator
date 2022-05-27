d3364cd9544aaa0156b78e22fe1e02789bc95a1b
lucykorea414 <31093793+lucykorea414@users.noreply.github.com>
2022-05-27 13:54:39 +0900

    Create Validate.cs

---
+++ SerialKey_test-preview/Validate.cs
@@ -0,0 +1,35 @@
+﻿using SKGL;
+
+namespace SerialKey_test_preview
+{
+    internal class Validate
+    {
+        SKGL.Validate val = new SKGL.Validate();
+
+        internal Validate(string password, string serialKey)
+        {
+            val.secretPhase = password;
+            val.Key = serialKey;
+        }
+
+        internal string val_creation()
+        {
+            return val.CreationDate.ToString();
+        }
+
+        internal string val_expiration()
+        {
+            return val.ExpireDate.ToString();
+        }
+
+        internal bool val_isValid()
+        {
+            return val.IsValid;
+        }
+
+        internal bool val_isExpired()
+        {
+            return val.IsExpired;
+        }
+    }
+}
